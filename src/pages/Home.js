78f7cb394732a4917871648e8a88b9b1824ebd73
y.gunes <yigit.gunes@partner.bmw.de>
2023-05-13 14:06:37 +0200

    [YG] project showcase implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom"
 import MountainImage from '../assets/images/mountain.jpg'
 import "../styles/Home.css"
+import Nestin from "../assets/images/house.jfif"
 
 function Home() {
     return (
@@ -13,7 +14,32 @@ function Home() {
                     <button className="contactButton"> Contact Now</button>
                 </Link>
             </div>
+            <div className={"projects"}>
+                <div className={"box"} style={{backgroundImage: `url(${Nestin})`}}>
+                    <h2 className={"projectTitle"}>Project 1</h2>
+                    <p className={"text"}>In this example, we're setting the .projectsContainer to be a flex container
+                        with a column direction and centered alignment. We also set some margins to separate it from the
+                        header.</p>
+                </div>
 
+                <div className={"box"}>
+                    <h2 className={"projectTitle"}>Project 2</h2>
+                    <p>In this example, we're setting the .projectsContainer to be a flex container with a column
+                        direction and centered alignment. We also set some margins to separate it from the header.</p>
+                </div>
+
+                <div className={"box"}>
+                    <h2 className={"projectTitle"}>Project 3</h2>
+                    <p>In this example, we're setting the .projectsContainer to be a flex container with a column
+                        direction and centered alignment. We also set some margins to separate it from the header.</p>
+                </div>
+
+                <div className={"box"}>
+                    <h2 className={"projectTitle"}>Project 4</h2>
+                    <p>In this example, we're setting the .projectsContainer to be a flex container with a column
+                        direction and centered alignment. We also set some margins to separate it from the header.</p>
+                </div>
+            </div>
         </div>
     )
 }
